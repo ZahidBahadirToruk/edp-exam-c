3956d17d0b7ad8292b4e062e179d4df268e4dd68
Z.Bahadir Toruk <150917933+ZahidBahadirToruk@users.noreply.github.com>
2025-01-15 13:02:58 +0000

    Add class

---
+++ main.py
@@ -0,0 +1,12 @@
+class Event:
+    def __init__(self, name, payload):
+        self.name = name
+        self.payload = payload
+
+class ApplicationSentEvent(Event):
+    def __init__(self, student_id, university_id):
+        super().__init__("application_sent", {"student_id": student_id, "university_id": university_id})
+
+class ApplicationApprovedEvent(Event):
+    def __init__(self, student_id, university_id):
+        super().__init__("application_approved", {"student_id": student_id, "university_id": university_id})
