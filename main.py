e120335b999228a0529df895d72fb2b269027a5e
Z.Bahadir Toruk <150917933+ZahidBahadirToruk@users.noreply.github.com>
2025-01-15 13:07:10 +0000

    Add class

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ class ApplicationSentEvent(Event):
 class ApplicationApprovedEvent(Event):
     def __init__(self, student_id, university_id):
         super().__init__("application_approved", {"student_id": student_id, "university_id": university_id})
+
+class ApplicationRejectedEvent(Event):
+    def __init__(self, student_id, university_id, reason):
+        super().__init__("application_rejected", {"student_id": student_id, "university_id": university_id, "reason": reason})
+
